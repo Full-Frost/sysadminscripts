71035bcdfd25dc6272ec0763008bb4826b357fec
Colin O'Rourke <50626696+Full-Frost@users.noreply.github.com>
2022-01-28 11:41:23 -0500

    Update ping_test.py

--- ping_test.py
+++ ping_test.py
@@ -2,7 +2,7 @@
 #Colin O'Rourke ping_test.py 2/4/2022
 import os
 import subprocess
-import time
+
 
 hostname = "www.google.com" #For testing DNS 
 remoteIP = "129.21.3.17" #RIT DNS 
@@ -15,7 +15,7 @@ def pingGoogle():
         print("Not able to connect to the remote IP")
 
 def pingIP():
-    pingTest = os.system("ping -c 1" + remoteIP)
+    pingTest = os.system("ping -c 1 " + remoteIP)
     if pingTest == 0:
         print("DNS is up")
     else:
@@ -23,37 +23,43 @@ def pingIP():
 
 def gatewayTest():
     addr = subprocess.Popen(["ip r"], stdout=subprocess.PIPE, shell=True)
-    output = addr.subprocess.read()
-    print("default gateway = " + output)
-    pingTest = os.system("ping -c 1" + output)
+    output = addr.stdout.read().decode()
+    sep = output.split(" ")
+    gate = sep[2]
+    print("default gateway = " + gate)
+    pingTest = os.system("ping -c 1 " + gate)
     if pingTest == 0:
         print("Able to connect to the default gateway")
     else:
         print("Not able to connect to the default gateway")
 
 def main():
+    os.system('clear')
     print('Welcome to the ping_test.py')
     print('The following options are available')
     print('|1| DNS test')
     print('|2| ping remote IP')
     print('|3| ping default gateway')
     print('|4| All 3 tests')
+    print('|5| to exit the program')
     while(1):
-        option = input("Please enter the number for the corresponding option you which to use.")
-        if(option == 1):
+        option = input("Please enter the number for the corresponding option you which to use. (1-5)")
+        if(option == '1'):
             pingGoogle()
-        elif(option == 2):
+        elif(option == '2'):
             pingIP()
-        elif(option == 3):
+        elif(option == '3'):
             gatewayTest()
-        elif(option == 4):
+        elif(option == '4'):
             pingGoogle()
             pingIP()
             gatewayTest()
+        elif(option == '5'):
+            quit()
         else:
-            print("Please enter a number 1-4")
-        time.sleep(500)
-        os.system('clear')
+            print("Please enter a number 1-5")
+        
+        
 
 if __name__ == "__main__":
     main()
