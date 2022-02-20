67fb21d54e6534f6372444dda399ce82deab281b
Colin O'Rourke <50626696+Full-Frost@users.noreply.github.com>
2022-02-19 19:51:51 -0500

    Update add_user.py

--- add_user.py
+++ add_user.py
@@ -1,14 +1,74 @@
 #!/bin/bash
 #Colin O'Rourke add_users.py 2/19/2022
+
 import os
 import csv
-import pwd
-import grp
+#import grp
 
 PASSWORD = 'password'       #default password 
-OFFICE_SHELL = '/bin/csh'   #office group shell
+OFFICESHELL = '/bin/csh'
+DEFAULTSHELL = '/bin/bash'
 
-def file_reader():#reads the file and returns a nested list of all the entries while ignoring the header line 
+def file_reader():
+    with open('linux_users.csv') as csv_file:
+        csvRead = csv.reader(csv_file, delimiter=',')#creates the list
+        for row in csvRead:
+            eid = row[0]
+            first = row[1]
+            last = row[2]
+            office = row[3]
+            phone = row[4]
+            department = row[5]
+            group = row[6]
+
+            if first == '' or last == '' or department == '' or department.isdigit():
+                print('Data entry error in the user at row '+ row)
+                continue
+        
+            useradder(first, last, eid, group, department)
+
+def usernameMaker(first, last):
+    usernames_d = {}
+    first = first[0]
+    username = first + last
+    if username not in usernames_d:
+        usernames_d[username] = 1
+    else:
+        count = usernames_d[username]
+        usernames_d[username] += 1
+        username + str(count)
+    return username
+
+def duplicateEID(eid):
+    eid_d = {}
+    if eid not in eid_d:
+        eid_d[eid] = 1
+    else:
+        count = eid[eid]
+        eid_d[eid] += 1
+        eid + str(count)
+
+def useradder(first, last, eid, group, deparment):
+    username = usernameMaker(first, last)
+    actualEID = duplicateEID(eid)
+    os.system('groupadd ' + group)
+    print('Adding group: '+ group)
+    os.system('mkdir -p /home/' + deparment + '/' + username)
+    print('Directory made: /home/' + deparment + '/' + username)
+    if group == 'office':
+        os.system('useradd -m -d /home/' + deparment + '/' + username + ' -s ' + OFFICESHELL + ' -g ' + group + ' -u ' + actualEID + username)
+        print('useradd -m -d /home/' + deparment + '/' + username + ' -s ' + OFFICESHELL + ' -g ' + group + ' -u ' + actualEID + username)
+    else:
+        os.system('useradd -m -d /home/' + deparment + '/' + username + ' -s ' + DEFAULTSHELL + ' -g ' + group + ' -u ' + actualEID + username)
+        print('useradd -m -d /home/' + deparment + '/' + username + ' -s ' + DEFAULTSHELL + ' -g ' + group + ' -u ' + actualEID + username)
+    os.system('echo' + PASSWORD + ':' + username + ' | chpasswd')
+    print('echo' + PASSWORD + ':' + username + ' | chpasswd')
+    os.system('passwd -e ' + username)
+    print('passwd -e ' + username)
+
+
+
+"""def file_reader():#reads the file and returns a nested list of all the entries while ignoring the header line 
     with open('linux_users.csv') as csv_file:
         csv_read = csv.reader(csv_file, delimiter=',')#creates the list
         counter = 0
@@ -18,106 +78,66 @@ def file_reader():#reads the file and returns a nested list of all the entries w
                 counter += 1
                 continue
             else:
-                #print(row)
+                print(row)
                 rows.append(row)#adding the lists into the main list
                 counter += 1
     #for i in rows:
-    #print(rows)
+    print(rows)
     return rows
 
-def group(rows): #Handles group creation 
-    #print(len(rows))
-    for i in range(7):
-        group_name = rows[i][6]#gets the group name 
-        try: #check to see if the group name exists 
-            grp.getgrnam(group_name)
-            print('group exists')
-        except KeyError:#if it doesn't exist create it 
-            #os.system('groupadd '+ group_name)
-            print('adding user group: '+ group_name)
-        
+#def group(rows): #Handles group creation 
+#    #print(len(rows))
+#    for i in range(7):
+#        group_name = rows[i][6]#gets the group name 
+#        try: #check to see if the group name exists 
+ #           grp.getgrnam(group_name)
+ #           print('group exists')
+ #       except KeyError:#if it doesn't exist create it 
+ #           #os.system('groupadd '+ group_name)
+ ##           print('adding user group: '+ group_name)
+  #      print()
     
-def username(rows): # creates the username from the nested list and puts all of them into one list does not account for duplication
-    usernames = []
-    for i in range(len(rows)):
-        try:
-            username = rows[i][2][0]+rows[i][1]#combines the first letter of the first name and the last name
-            usernames.append(username)
-        except IndexError:
-            print("invalid username entry")
-            usernames.append("INVALID")
-    return usernames
+#def username(rows): # creates the username from the nested list and puts all of them into one list does not account for duplication
+#    usernames = []
+#    for i in range(len(rows)):
+#        username = rows[i][2][0]+rows[i][1]#combines the first letter of the first name and the last name
+#        usernames.append(username)
+#    return usernames
 
-def userid(rows):# creates the userids from the nested list and puts all of them into one list does not account for duplication
-    userids = []
-    for i in range(len(rows)):
-        try:
-            user_id = rows[i][0]#retrieves the user id
-            userids.append(user_id)
-        except IndexError:
-            print("invalid userid entry")
-            user_id.append("INVALID")
-    return userids
+#def userid(rows):# creates the userids from the nested list and puts all of them into one list does not account for duplication
+#    userids = []
+#    for i in range(len(rows)):
+#        user_id = rows[i][0]#retrieves the user id
+#        userids.append(user_id)
+#    return userids
 
-def duplicateUsername(usernames):#handles duplicate usernames 
-    usernames_d = {}#store the frequency of the strings 
-    for x in range(0, len(usernames)):#iterates over the list
-        if usernames[x] != 'INVALID': 
-            if usernames[x] not in usernames_d:#checks to see if the username has appeared before, if it hasnt adds it into the dictonary
-                usernames_d[usernames[x]] = 1
-            else:#username already within the dictionary 
-                count = usernames_d[usernames[x]]#counts how many duplicates
-                usernames_d[usernames[x]] += 1
-                usernames[x] += str(count)#adds number to the username 
-        else:
-            continue
-    return usernames#returns the parameter list
+#def duplicateUsername(usernames):#handles duplicate usernames 
+#    usernames_d = {}#store the frequency of the strings 
+ #   for x in range(0, len(usernames)):#iterates over the list
+#        if usernames[x] not in usernames_d:#checks to see if the username has appeared before, if it hasnt adds it into the dictonary
+ #           usernames_d[usernames[x]] = 1
+#        else:#username already within the dictionary 
+#            count = usernames_d[usernames[x]]#counts how many duplicates
+ #           usernames_d[usernames[x]] += 1
+ #           usernames[x] += str(count)#adds number to the username 
+ #   return usernames#returns the parameter list
 
-def duplicateUserID(userids):
-    userids_d = {}#store the frequency of the strings
-    for x in range(0, len(userids)):#iterates over the list
-        if userids[x] != 'INVALID':
-            if userids[x] not in userids_d:#checks to see if the userID has appeared before, if it hasnt adds it into the dictonary
-                userids_d[userids[x]] = 1
-            else:#userID is already within the dictionary 
-                count = userids_d[userids[x]]#counts how many duplicates
-                userids_d[userids[x]] += 1
-                userids[x] += str(count)#adds number to the userID
-        else:
-            continue
-    return userids#returns the parameter list
+#def duplicateUserID(userids):
+ #   userids_d = {}#store the frequency of the strings
+ #   for x in range(0, len(userids)):#iterates over the list
+#        if userids[x] not in userids_d:#checks to see if the userID has appeared before, if it hasnt adds it into the dictonary
+#            userids_d[userids[x]] = 1
+ #       else:#userID is already within the dictionary 
+ #           count = userids_d[userids[x]]#counts how many duplicates
+#            userids_d[userids[x]] += 1
+ #           userids[x] += str(count)#adds number to the userID
+ #   return userids#returns the parameter list
 
-def finale(userids, usernames, rows):
-    for i in range(len(rows)):#iterate through all the rows also used to determine what the username/userid is 
-        group_name = rows[i][6]#get the groupname 
-        department = rows[i][5]#get the department 
-        try:
-            if department[0].isdigit():#check to make sure the department is not a number 
-                department == 'INVALID'
-        except IndexError:#if the department field is empty will raise error 
-            print("invalid department entry")
-            department == 'INVALID'
-        for x in range(7):
-            if rows[i][x] == 'INVALID' or usernames[i] == 'INVALID' or userids[i] == 'INVALID':#checks to make sure the user data was correct
-                print("User was not able to be created due to improper data entry.")#message to user about improper data
-            else:
-                directory = '/home/' + department + '/' + usernames[i]
-                os.system("mkdir -p "+ directory)#creates the directory 
-                if group_name == 'office':#checks to see if the group name is office
-                    os.system("useradd -m -d "+ directory + ' -g ' + group_name + ' ' + usernames[i])
-                    os.system('echo' + PASSWORD + ':' + usernames[i] + ' | chpasswd')
-                    os.system('passwd -e ' + username[i])
-                    os.system('usermod --shell ' + OFFICE_SHELL + ' ' + usernames[i])#sets the default shell to /bin/csh
-                else:#every other group is added normally below 
-                    os.system("useradd -m -d "+ directory + ' -g ' + group_name + ' ' + usernames[i])
-                    os.system('echo' + PASSWORD + ':' + usernames[i] + ' | chpasswd')
-                    os.system('passwd -e ' + username[i])
-
+#def finale(userids, usernames, group):
+#    pass
 
 def main():
-    os.system('clear')
-    finale(duplicateUserID(userid(file_reader())), duplicateUsername(username(file_reader())), file_reader())
+    #os.system('clear')
+    file_reader()
 
-
-
-main()
+main()"""
